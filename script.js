c638dda0189ba8248bef8a2738d2eb640b899fe3
vsingh80421-9669 <vsingh80421@gmail.com>
2026-07-11 18:15:11 +0530

    Add event listener to calculation button
    Added event listener for calculation button.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const followersInput = document.getElementById('followers-input');
-const followersInput = document.getElementById('followers-input');
 const likesInput = document.getElementById('likes-input');
 const calcBtn = document.getElementById('calc-btn');
+
+calcBtn.addEventListener('click' ,function() {
+    // Asli calculations iske andar aayengi
+});
