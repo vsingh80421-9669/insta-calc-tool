586245bd92cb86026131974909178aa371e1cfd9
vsingh80421-9669 <vsingh80421@gmail.com>
2026-07-17 16:32:17 +0530

    Implement engagement rate health indicators
    Added health text display based on engagement rate.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const followersInput = document.getElementById('followers-input');
 const likesInput = document.getElementById('likes-input');
 const calcBtn = document.getElementById('calc-btn');
 const resText = document.getElementById('res-text');
+const healthText = document.getElementById('health-text');
 
 calcBtn.addEventListener('click' ,function() {
    const followers = Number(followersInput.value);
@@ -15,5 +16,12 @@ calcBtn.addEventListener('click' ,function() {
     const engagement = (like / followers) * 100;
         alert("Aapka Engagement Rate hai: " + engagement.toFixed(2) + "%");
     resText.innerText = engagement.toFixed(2) + "%";
+
+   if (engagement < 3) {
+      healthText.innerText = "Low";
+   } else if (engagement <= 6) {
+      healthText.innerText = "Good"; 
+   } else {
+      healthText.innerText = "Excellent";
 });
                          
